e97686a9ac332edac644bcff91dfa88994ebc63f
werksimon1 <werksimon@gmail.com>
2017-10-11 21:13:21 +0200

    Add files via upload

---
+++ templateliterals.js
@@ -0,0 +1,27 @@
+
+// Single line
+let greeting = `Hello`;
+
+
+// Multi line
+
+let helloWorld = `Hello
+World`;
+console.log(helloWorld);
+
+var helloWorldOld = 'Hello\nWorld';
+console.log(helloWorldOld);
+
+// Expressions
+let course = `ES6`;
+const url = 'https://developer.mozilla.org/en-US/';
+console.log (`Cursus: ${course}`);
+
+const es6 = `<article>
+
+    <h3>${course} is the new standard of JavaScript</h3>
+    <p>You can find more at ${url}.</p>
+
+</article>`;
+
+document.write(es6);
